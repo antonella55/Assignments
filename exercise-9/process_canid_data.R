7699469337540510fc63ef669cc0b6b995a56c39
Antonella Gonzalez <antonella55@g.ucla.edu>
2017-03-15 15:06:11 -0700

    ex9

---
+++ exercise-9/process_canid_data.R
@@ -0,0 +1,15 @@
+---
+title: "Untitled"
+author: "Antonella Gonzalez"
+date: "March 8, 2017"
+output: html_document
+---
+
+```{r}
+source("~/PyRate/pyrate_utilities.r")
+
+extant_dogs = c("Canis rufus","Canis lupus","Canis aureus","Canis latrans","Canis mesomelas","Canis anthus","Pseudalopex gymnocercus","Canis adustus","Canis familiaris")
+
+extract.ages.pbdb(file= "canid_occ.csv",extant_species=extant_dogs)
+```
+
